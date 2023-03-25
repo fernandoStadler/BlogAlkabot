d6f6fb8cc30fce2ecf523b3c83167239c2beb3ea
Fernando Stadler <fernandostadler2@gmail.com>
2023-03-25 03:50:36 -0300

    Alterada chamada da api de noticias

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,42 +1,46 @@
 import { useState, useEffect } from "react";
-import axios from "axios";
-import { HomeContainer, NewsContainer, NewsCard, NewsTumb, NewsTitle, NewsDescription, NewsButton,Pagnination } from "../styles/styledHome";
+import { HomeContainer, NewsContainer, NewsCard, NewsTumb, NewsTitle, NewsDescription, NewsButton, Pagnination } from "../styles/styledHome";
 import Menu from "../components/Menu";
 import Footer from '../components/Footer';
 import Image from 'next/image';
 
 export default function Home() {
   const [news, setNews] = useState([]);
-
-  const [currentPage,setCurrentPage] = useState(1);
+  const [currentPage, setCurrentPage] = useState(1);
   const [newsPerPage, setNewsPerPage] = useState(4);
-  
+
   const indexOfLastNew = currentPage * newsPerPage;
   const indexOfFirstNew = indexOfLastNew - newsPerPage;
   const currentNews = news.slice(indexOfFirstNew, indexOfLastNew);
 
   const totalPages = Math.ceil(news.length / newsPerPage);
   const pageNumbers = [];
+
   useEffect(() => {
-    axios
-      .get("https://newsapi.org/v2/top-headlines?country=br&category=technology&apiKey=484e6a3d0050495193db5d20649e6c85")
-      .then((response) => {
-        setNews(response.data.articles);
+    fetch("https://newsapi.org/v2/top-headlines?country=br&category=technology&apiKey=484e6a3d0050495193db5d20649e6c85")
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Erro ao obter notícias.');
+        }
+        return response.json();
       })
-      .catch((error) => {
+      .then(data => {
+        setNews(data.articles);
+      })
+      .catch(error => {
         console.error(error);
       });
   }, []);
 
   for (let i = 1; i <= totalPages; i++) {
     pageNumbers.push(i);
-}
+  }
 
+  const handleClick = (event, pageNumber) => {
+    event.preventDefault();
+    setCurrentPage(pageNumber);
+  };
 
-const handleClick = (event, pageNumber) => {
-  event.preventDefault();
-  setCurrentPage(pageNumber);
-};
 
   return (
     <>
@@ -75,16 +79,16 @@ const handleClick = (event, pageNumber) => {
         </NewsContainer>
       </HomeContainer>
       <Pagnination>
-                {pageNumbers.map(number => (
-                    <a
-                        href="#"
-                        key={number}
-                        onClick={event => handleClick(event, number)}
-                    >
-                        {number}
-                    </a>
-                ))}
-            </Pagnination>
+        {pageNumbers.map(number => (
+          <a
+            href="#"
+            key={number}
+            onClick={event => handleClick(event, number)}
+          >
+            {number}
+          </a>
+        ))}
+      </Pagnination>
       <Footer />
     </>
   );
